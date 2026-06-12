068023e526ff2b068787c13057ea9688c6d7bc80
maxytmaxyt <163127355+maxytmaxyt@users.noreply.github.com>
2026-06-12 17:29:19 +0200

    Update ConfigManager.h

--- Shared/ConfigManager.h
+++ Shared/ConfigManager.h
@@ -1,14 +1,24 @@
 #pragma once
+#include <windows.h>
 #include <string>
+#include <shlobj.h> // For SHGetFolderPath
 
-// Header-only helper to manage the shared configuration file
 class ConfigManager {
-public:
-    static void SaveSetting(std::string key, std::string value) {
-        // Write to %APPDATA%\GameBooster\config.ini
+    static std::wstring GetConfigPath() {
+        wchar_t path[MAX_PATH];
+        SHGetFolderPathW(NULL, CSIDL_APPDATA, NULL, 0, path);
+        std::wstring fullPath = std::wstring(path) + L"\\GameBooster\\config.ini";
+        return fullPath;
     }
-    static std::string GetSetting(std::string key) {
-        // Read from %APPDATA%\GameBooster\config.ini
-        return "";
+
+public:
+    static void SaveSetting(std::wstring key, std::wstring value) {
+        WritePrivateProfileStringW(L"Settings", key.c_str(), value.c_str(), GetConfigPath().c_str());
+    }
+
+    static std::wstring GetSetting(std::wstring key) {
+        wchar_t buffer[256];
+        GetPrivateProfileStringW(L"Settings", key.c_str(), L"", buffer, 256, GetConfigPath().c_str());
+        return std::wstring(buffer);
     }
 };
