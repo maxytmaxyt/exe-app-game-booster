d973d17520bc6c47a727dd5b2cdbf90d9a731ea8
maxytmaxyt <163127355+maxytmaxyt@users.noreply.github.com>
2026-06-12 17:28:06 +0200

    Create main.cpp

---
+++ ConfigUI/src/main.cpp
@@ -0,0 +1,7 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    // Simple window creation logic here
+    MessageBox(NULL, "GameBooster UI Initialized", "Status", MB_OK);
+    return 0;
+}
